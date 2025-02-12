08e76828e6c61ffbe4c374683b37615e1b3ea45a
gleiMoura <gle.bar.mou@gmail.com>
2025-02-12 15:32:35 -0300

    🔥 remove token boolean

--- src/repository/filesRepository.ts
+++ src/repository/filesRepository.ts
@@ -3,9 +3,9 @@ import { bucket, bucketName } from "../config/cloud.js";
 // Set para controlar arquivos que estão em processo de upload
 const uploadInProgress = new Set();
 
-export const generateProfileLink = async (file, token: string) => {
+export const generateProfileLink = async (file) => {
     try {
-        if (!file || !file.path || !file.originalname || !token) {
+        if (!file || !file.path || !file.originalname) {
             throw new Error("File or token is not valid or missing required properties");
         }
 
