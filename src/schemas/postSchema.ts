f8c00412703a6281fb8d8e9d83ed32d6df2a7068
gleiMoura <gle.bar.mou@gmail.com>
2025-02-11 21:03:00 -0300

    🔥 remove unused files

--- src/schemas/postSchema.ts
+++
@@ -1,8 +0,0 @@
-import { registerType } from "../interfaces/index.js";
-import joi, { ObjectSchema } from "joi";
-
-const postSchema: ObjectSchema<registerType> = joi.object({
-    caption: joi.string().required(),
-});
-
-export default postSchema;
