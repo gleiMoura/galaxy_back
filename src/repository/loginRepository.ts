c01274c6ba6b6f3ea0e7b6d44c59c21224cb1124
gleiMoura <gle.bar.mou@gmail.com>
2025-02-12 15:05:50 -0300

    ♻️ refactor: update login to find user with email

--- src/repository/loginRepository.ts
+++ src/repository/loginRepository.ts
@@ -1,41 +1,16 @@
-import database from "../config/index.js"
-import { ObjectId } from "mongodb";
+import prisma from "config";
 
-const findUser = async (email: string) => {
+const findUser = async (email) => {
     try {
-        const db = await database;
-        const user = await db.collection('users').findOne({ email });
-
-        return user;
+        return (
+            await prisma.student.findUnique({ where: { email } }) ||
+            await prisma.teacher.findUnique({ where: { email } })
+        );
     } catch (error) {
-        console.log(error)
+        console.error("Error finding user:", error);
     }
 };
 
-const findUserBySession = async (token: string) => {
-    try {
-        const db = await database;
-        const user = await db.collection('sessions').findOne({ token });
-
-        return user;
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-const startSession = async (userId: ObjectId, token: string) => {
-    try {
-        const db = await database;
-        await db.collection("sessions").insertOne({
-            userId, token
-        })
-    } catch (error) {
-        console.log("Error trying start user session", error)
-    }
-}
-
 export {
-    findUser,
-    startSession,
-    findUserBySession
+    findUser
 }
