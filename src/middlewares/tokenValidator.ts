6fa853961c1b6e1441002973e800c08eded0a1fb
gleiMoura <gle.bar.mou@gmail.com>
2025-02-11 23:28:30 -0300

    ✨ feat: create a meddleware to validate token by jwt

---
+++ src/middlewares/tokenValidator.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const authenticateToken = (req: Request, res: Response, next: NextFunction): void => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+        res.status(401).json({ message: "Access denied. No token provided." });
+        return; 
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(403).json({ message: "Invalid token." });
+    }
+};
+
+export default authenticateToken;
