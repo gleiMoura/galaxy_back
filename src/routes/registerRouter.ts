5d4c72c670168eeb2624ff7251de6b41c58d03c8
gleiMoura <gle.bar.mou@gmail.com>
2025-02-11 23:29:08 -0300

    ♻️ refactor: update register router

--- src/routes/registerRouter.ts
+++ src/routes/registerRouter.ts
@@ -1,14 +1,19 @@
 import { doRegister, insertProfileImage } from "../controllers/registerController.js";
 import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator.js";
-import registerSchema from "../schemas/registerSchema.js";
+import authenticateToken from "middlewares/tokenValidator.js";
+import registerSchemaStudent from "../schemas/studentRegisterSchema.js";
+import registerSchemaTeacher from "../schemas/teacherRegisterSchema.js";
 import multer from "multer"
 
 const upload = multer({ dest: 'uploads/' });
 
 const registerRouter = Router();
 
-registerRouter.post('/register', schemaValidator(registerSchema), doRegister)
-registerRouter.put('/register/profile', upload.single('file'), insertProfileImage)
+registerRouter.post('/register/teacher', schemaValidator(registerSchemaTeacher), doRegister)
+registerRouter.put('/register/profile/teacher', authenticateToken, upload.single('file'), insertProfileImage)
+
+registerRouter.post('/register/student', schemaValidator(registerSchemaStudent), doRegister)
+registerRouter.put('/register/profile/student', upload.single('file'), insertProfileImage)
 
 export default registerRouter;
