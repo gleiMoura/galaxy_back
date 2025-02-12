7874200a8d10f06114cd223f3d50394f59eeb12f
gleiMoura <gle.bar.mou@gmail.com>
2025-02-12 15:05:26 -0300

    ♻️ refactor: update repository to save student and teacher

--- src/repository/registerRepository.ts
+++ src/repository/registerRepository.ts
@@ -1,28 +1,49 @@
-import { ObjectId } from "mongodb";
-import database from "../config/index.js"
-import { registerType } from "../interfaces/index.js";
+import prisma from "config/index.js";
+import { studentRegisterType, teacherRegisterType } from "interfaces";
 
-export const logUserInDb = async (credentials: registerType) => {
+export const logUserInDb = async (userData) => {
     try {
-        const db = await database;
-        await db.collection('users').insertOne(credentials)
+        if (userData.role === "Teacher") {
+            return await prisma.teacher.create({
+                data: userData
+            });
+        } else if (userData.role === "Student") {
+            return await prisma.student.create({
+                data: {
+                    ...userData, interests: {
+                        create: userData.interests.map(subject => ({
+                            subject
+                        }))
+                    }
+                }
+            });
+        } else {
+            return null
+        }
     } catch (error) {
-        console.log("Error trying log user in db", error);
+        console.error("Problem in repository trying log user!", error);
     }
 };
 
-export const updateUserInDb = async (userId: string, profileLink: string) => {
+export const updateUserInDb = async (user: studentRegisterType | teacherRegisterType, profileLink: string) => {
     try {
-        const db = await database;
-        const userObjectId = new ObjectId(userId);
-        const result = await db.collection('users').updateOne(
-            { _id: userObjectId },
-            { $set: { profileUrl: profileLink } },
-            { writeConcern: { w: 1 } }
-        );
+        let result = null;
+
+        if (user.role === "student") {
+            result = await prisma.student.update({
+                where: { email: user.email },
+                data: { profileUrl: profileLink },
+            });
+        } else {
+            result = await prisma.teacher.update({
+                where: { email: user.email },
+                data: { profileUrl: profileLink },
+            });
+        }
 
         return result;
+
     } catch (error) {
-        console.log("Error trying log user in db", error);
+        console.error("Error updating user profile in DB:", error);
     }
-}
+};
