84cdc1ad4652657a97761a454c7047fe239c0d72
gleiMoura <gle.bar.mou@gmail.com>
2025-02-11 21:04:20 -0300

    🔧 config: create config to prisma

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,20 +1,20 @@
-import { MongoClient } from "mongodb";
+import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 import chalk from "chalk";
 
 dotenv.config()
 
-const Client = process.env.MONGO_URL + '&ssl=true&tlsAllowInvalidCertificates=true';
+const prisma = new PrismaClient();
 
 async function init() {
-  const mongoClient = new MongoClient(Client);
-  await mongoClient.connect();
-  console.log(chalk.green("Database is connected!"));
-  const database = mongoClient.db(process.env.DATABASE);
-
-  return database;
+  try {
+    await prisma.$connect();
+    console.log(chalk.green("Database is connected!"))
+  } catch (error) {
+    console.error(chalk.red("Error connecting to the database:", error))
+  }
 }
 
-const db = init();
+init();
 
-export default db;
+export default prisma;
