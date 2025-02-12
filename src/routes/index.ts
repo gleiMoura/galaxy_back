d01a4c383f73d4eb13a6dd7177b3abfde3fb5725
gleiMoura <gle.bar.mou@gmail.com>
2025-02-11 23:28:50 -0300

    ♻️ refactor: update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,9 @@
 import Router from "express";
 import loginRouter from "./loginRouter.js";
 import registerRouter from "./registerRouter.js";
-import quizzRouter from "./quizzRouter.js";
-import postsRouter from "./postsRouter.js";
-
 
 const router = Router();
 router.use(loginRouter);
 router.use(registerRouter);
-router.use(quizzRouter);
-router.use(postsRouter)
 
 export default router;
