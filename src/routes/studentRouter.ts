422664a4f5c7dcd761109a7cc6e40b17b14b5e23
gleiMoura <gle.bar.mou@gmail.com>
2025-02-13 00:25:30 -0300

    ✨ feat: create a student routes

---
+++ src/routes/studentRouter.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { getStudent } from "controllers/studentController";
+import { getStudents } from "controllers/studentController";
+import schemaValidator from "../middlewares/schemaValidator.js";
+import tokenValidator from "middlewares/tokenValidator.js";
+import studentSchema from "schemas/studentSchema.js";
+const studentRouter = Router();
+
+//students routes
+studentRouter.get("/student", tokenValidator, getStudent);
+studentRouter.get("/students", tokenValidator, getStudents);
+//studentRouter.put("/student", schemaValidator(studentSchema) updateStudent);
+//studentRouter.delete("/student", deleteStudent);
+//contract routes
+//studentRouter.get('/student/contract', getContracts)
+//studentRouter.post('/student/contract', schemaValidator(contractSchema), createContract);
+//studentRouter.put('/student/contract', schemaValidator(contractSchema), updateContract);
+//studentRouter.delete('/student/contract', deleteContract);
+//classes routes
+//studentRouter.post('/student/class', createClass);
+//studentRouter.get('/student/class', getClass);
+//studentRouter.get('/student/classes', getClasses);
+
+
+
+export default studentRouter;
