f5e31c879c7797b78bb7456dca32c081304a1e80
gleiMoura <gle.bar.mou@gmail.com>
2025-02-13 21:34:24 -0300

    ✨ feat: user student router!

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import Router from "express";
 import loginRouter from "./loginRouter.js";
 import registerRouter from "./registerRouter.js";
+import studentRouter from "./studentRouter.js";
 
 const router = Router();
 router.use(loginRouter);
 router.use(registerRouter);
+router.use(studentRouter)
 
 export default router;
