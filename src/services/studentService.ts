300b79dab8d041b81da71c5630b4054bbfb33aaa
gleiMoura <gle.bar.mou@gmail.com>
2025-02-14 21:44:39 -0300

    ✨ feat: create service function to delete student

--- src/services/studentService.ts
+++ src/services/studentService.ts
@@ -1,5 +1,5 @@
 import { studentType } from "interfaces";
-import { findUser, findAllStudents, updateStudentInDB } from "../repository/studentRepository";
+import { findUser, findAllStudents, updateStudentInDB, deleteStudentInDb } from "../repository/studentRepository";
 
 export const findStudent = async (email: string) => {
     const user = await findUser(email);
@@ -66,4 +66,28 @@ export const updateUserStudent = async (email: string, updateData: studentType) 
     const users = await updateStudentInDB(userId, updateData)
 
     return users;
+};
+
+export const deleteUserStudent = async (email: string, studentId) => {
+    const adminUser = await findUser(email);
+
+    if (adminUser.role !== "admin") {
+        throw {
+            response: {
+                status: 400,
+                message: "User doen't have permission!"
+            }
+        }
+    }
+
+    const result = await deleteStudentInDb(studentId)
+
+    if (!result) {
+        throw {
+            response: {
+                status: 500,
+                message: "It was not possible delete user at the moment."
+            }
+        }
+    }
 };
