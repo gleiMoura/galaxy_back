77027aaee760a4f78d73d68e4340576390da1056
gleiMoura <gle.bar.mou@gmail.com>
2025-02-15 00:54:29 -0300

    ♻️ refactor: separate routes: student, contracts and classes

--- src/routes/studentRouter.ts
+++ src/routes/studentRouter.ts
@@ -1,27 +1,12 @@
 import { Router } from "express";
-import { deleteStudent, getStudent, updateStudent } from "controllers/studentController";
-import { getStudents } from "controllers/studentController";
-import schemaValidator from "../middlewares/schemaValidator.js";
+import { deleteStudent, getStudent, updateStudent, getStudents } from "controllers/studentController";
 import tokenValidator from "middlewares/tokenValidator.js";
-import schemas from "schemas/index.js";
 
 const studentRouter = Router();
 
-//students routes
 studentRouter.get("/student", tokenValidator, getStudent);
 studentRouter.get("/students", tokenValidator, getStudents);
 studentRouter.put("/student", tokenValidator, updateStudent);
 studentRouter.delete("/student/:id", tokenValidator, deleteStudent);
-//contract routes
-studentRouter.post('/student/contract', schemaValidator(schemas.contractSchema), createContract);
-//studentRouter.get('/student/contract', getContracts)
-//studentRouter.put('/student/contract', schemaValidator(contractSchema), updateContract);
-//studentRouter.delete('/student/contract', deleteContract);
-//classes routes
-//studentRouter.post('/student/class', createClass);
-//studentRouter.get('/student/class', getClass);
-//studentRouter.get('/student/classes', getClasses);
-
-
 
 export default studentRouter;
