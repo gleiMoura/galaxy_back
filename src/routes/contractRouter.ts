8124061cd51b0829e5ffda59fb0005722975513d
gleiMoura <gle.bar.mou@gmail.com>
2025-02-16 10:59:32 -0300

    ✨ feat: create a router to get all contracts

--- src/routes/contractRouter.ts
+++ src/routes/contractRouter.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator.js";
 import schemas from "schemas/index.js";
-import { createContract } from "controllers/contractController.js";
+import { createContract, getContracts } from "controllers/contractController.js";
 import tokenValidator from "middlewares/tokenValidator.js";
 
 const contractRouter = Router();
 
 contractRouter.post('/contract', schemaValidator(schemas.contractSchema), tokenValidator, createContract);
-//studentRouter.get('/student/contract', getContracts)
-//studentRouter.put('/student/contract', schemaValidator(contractSchema), updateContract);
-//studentRouter.delete('/student/contract', deleteContract);
+contractRouter.get('/contract', tokenValidator, getContracts)
+//contractRouter.put('contract', schemaValidator(contractSchema), updateContract);
+//contractRouter.delete('/contract', deleteContract);
 
 export default contractRouter;
