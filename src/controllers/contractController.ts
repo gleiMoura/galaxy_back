6e1b4a9f12271742d92705277d13b21b5d672177
gleiMoura <gle.bar.mou@gmail.com>
2025-02-26 14:02:36 -0300

    ✨ feat: create a controller function to update contract

--- src/controllers/contractController.ts
+++ src/controllers/contractController.ts
@@ -1,6 +1,7 @@
 
 import { Request, Response } from "express";
-import { getAllContracts, makeContract } from "services/contractService";
+import { ContractType } from "interfaces";
+import { changeContract, getAllContracts, makeContract } from "services/contractService";
 
 export const createContract = async (req: Request, res: Response) => {
     const userEmail = req.user?.email;
@@ -18,3 +19,13 @@ export const getContracts = async (req: Request, res: Response) => {
 
     res.send(contracts).status(200);
 };
+
+export const updateContract = async (req: Request, res: Response) => {
+    const adminEmail = req.user?.email;
+    const dataContract: ContractType = req.body;
+
+    const contract = await changeContract(adminEmail, dataContract)
+
+    res.send(contract).status(200);
+};
+
