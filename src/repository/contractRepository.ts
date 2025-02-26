e6f5c7d1a7d27d96b57f801d051e73b9b92eca3a
gleiMoura <gle.bar.mou@gmail.com>
2025-02-26 14:02:49 -0300

    ✨ feat: create a repository function to update contract

--- src/repository/contractRepository.ts
+++ src/repository/contractRepository.ts
@@ -1,4 +1,5 @@
 import prisma from "config";
+import { ContractType } from "interfaces";
 
 export const createContractInDb = async (data) => {
     try {
@@ -16,6 +17,19 @@ export const getContractsInDb = async () => {
     try {
         return (
             await prisma.contract.findMany()
+        );
+    } catch (error) {
+        console.error("Error finding user:", error);
+    }
+};
+
+export const changeContractInDb = async (dataContract: ContractType) => {
+    try {
+        return (
+            await prisma.contract.update({
+                data: { ...dataContract },
+                where: { id: dataContract.id }
+            })
         );
     } catch (error) {
         console.error("Error finding user:", error);
