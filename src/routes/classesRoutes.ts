099527f6ece2aa0a30d5ade8547614a5adf7b9b4
gleiMoura <gle.bar.mou@gmail.com>
2025-02-26 13:28:16 -0300

    ✨ feat: create classes router

---
+++ src/routes/classesRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const classesRouter = Router();
+
+//classesRouter.get('/student/class', getClass);
+//classesRouter.post('/student/class', createClass);
+//classesRouter.get('/student/classes', getClasses);
+
+export default classesRouter;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import loginRouter from "./loginRouter.js";
 import registerRouter from "./registerRouter.js";
 import studentRouter from "./studentRouter.js";
 import contractRouter from "./contractRouter.js";
+import classesRouter from "./classesRoutes.js";
 
 const router = Router();
 router.use(loginRouter);
 router.use(registerRouter);
-router.use(studentRouter)
-router.use(contractRouter)
+router.use(studentRouter);
+router.use(contractRouter);
+router.use(classesRouter);
 
 export default router;
