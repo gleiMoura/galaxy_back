4d6fc9c6366db6db25bb429bbf9ce813ec6423bd
gleiMoura <gle.bar.mou@gmail.com>
2025-02-26 14:03:01 -0300

    ✨ feat: create a router function to update contract

--- src/routes/contractRouter.ts
+++ src/routes/contractRouter.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator.js";
 import schemas from "schemas/index.js";
-import { createContract, getContracts } from "controllers/contractController.js";
+import { createContract, getContracts, updateContract } from "controllers/contractController.js";
 import tokenValidator from "middlewares/tokenValidator.js";
 
 const contractRouter = Router();
 
 contractRouter.post('/contract', schemaValidator(schemas.contractSchema), tokenValidator, createContract);
 contractRouter.get('/contract', tokenValidator, getContracts)
-//contractRouter.put('contract', schemaValidator(contractSchema), updateContract);
+contractRouter.put('/contract', schemaValidator(schemas.contractSchema), tokenValidator, updateContract);
 //contractRouter.delete('/contract', deleteContract);
 
 export default contractRouter;
