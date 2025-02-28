5cee7ab7c6ca8653f07b26eb2ae9927196fd4bba
gleiMoura <gle.bar.mou@gmail.com>
2025-02-27 23:32:52 -0300

    ✨ create router to delete a contract

--- src/routes/contractRouter.ts
+++ src/routes/contractRouter.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator.js";
 import schemas from "schemas/index.js";
-import { createContract, getContracts, updateContract } from "controllers/contractController.js";
+import { createContract, deleteContract, getContract, getContracts, updateContract } from "controllers/contractController.js";
 import tokenValidator from "middlewares/tokenValidator.js";
 
 const contractRouter = Router();
 
 contractRouter.post('/contract', schemaValidator(schemas.contractSchema), tokenValidator, createContract);
-contractRouter.get('/contract', tokenValidator, getContracts)
+contractRouter.get('/contracts', tokenValidator, getContracts);
+contractRouter.get('/contract/:id', tokenValidator, getContract);
 contractRouter.put('/contract', schemaValidator(schemas.contractSchema), tokenValidator, updateContract);
-//contractRouter.delete('/contract', deleteContract);
+contractRouter.delete('/contract/:id', tokenValidator, deleteContract);
 
 export default contractRouter;
