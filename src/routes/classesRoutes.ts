c3ad4d4ec254fc8ef88b90e67f7a39619af922b4
gleiMoura <gle.bar.mou@gmail.com>
2025-03-02 10:18:18 -0300

    ✨ ✨ feat: create a router function to class

--- src/routes/classesRoutes.ts
+++ src/routes/classesRoutes.ts
@@ -1,9 +1,11 @@
+import { createClass } from "controllers/classController";
 import { Router } from "express";
+import tokenValidator from "middlewares/tokenValidator";
 
 const classesRouter = Router();
 
+classesRouter.post('/student/class', tokenValidator, createClass);
 //classesRouter.get('/student/class', getClass);
-//classesRouter.post('/student/class', createClass);
 //classesRouter.get('/student/classes', getClasses);
 
 export default classesRouter;
