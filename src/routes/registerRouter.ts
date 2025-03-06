d3b08c9ad5656fafddcb74a02597a90cc30bfcbe
gleiMoura <gle.bar.mou@gmail.com>
2025-03-06 01:05:51 -0300

    ♻️ refactor: add token validator to registerRouter

--- src/routes/registerRouter.ts
+++ src/routes/registerRouter.ts
@@ -1,7 +1,7 @@
 import { doRegister, insertProfileImage } from "../controllers/registerController.js";
 import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator.js";
-import authenticateToken from "middlewares/tokenValidator.js";
+import tokenValidator from "middlewares/tokenValidator.js";
 import schemas from "schemas/index.js";
 import multer from "multer"
 
@@ -12,6 +12,6 @@ const registerRouter = Router();
 registerRouter.post('/register/teacher', schemaValidator(schemas.teacherRegisterSchema), doRegister);
 registerRouter.post('/register/student', schemaValidator(schemas.studentRegisterSchema), doRegister);
 registerRouter.post('/register/admin', schemaValidator(schemas.adminRegisterSchema), doRegister);
-registerRouter.put('/register/profile', authenticateToken, upload.single('file'), insertProfileImage)
+registerRouter.put('/register/profile', tokenValidator, upload.single('file'), insertProfileImage)
 
 export default registerRouter;
