1f47ec1d27ea227e5a8ea20cf117a720dc7eff03
gleiMoura <gle.bar.mou@gmail.com>
2025-03-06 01:01:48 -0300

    ✨ feat: create a controller function to class

--- src/controllers/classController.ts
+++ src/controllers/classController.ts
@@ -1,15 +1,15 @@
 import { generateFileLink } from "repository/filesRepository";
 import { Request, Response } from "express";
 import { generateClass } from "services/classService";
+import { createFileLink } from "services/fileService";
 
 export const createClass = async (req: Request, res: Response) => {
     const file = req.file;
-    const data = req.body.data;
     const userEmail = req.user?.email;
+    const data = req.body;
 
-    const fileUrl = await generateFileLink(file);
-    const newData = { ...data, fileUrl }
-    const newClass = await generateClass(userEmail, newData);
+    const fileUrl = await createFileLink(file);
+    const newClass = await generateClass(userEmail, data, fileUrl);
 
     res.status(200).send(newClass);
 };
