88a846563892af3128f98b038e0d57bd51808c05
gleiMoura <gle.bar.mou@gmail.com>
2025-03-06 15:30:12 -0300

    ♻️ refactor: update class schema

--- src/schemas/classSchema.ts
+++ src/schemas/classSchema.ts
@@ -2,6 +2,7 @@ import Joi from "joi";
 
 const classSchema = Joi.object({
     studentId: Joi.number().integer().required(),
+    teacherId: Joi.number().integer().required(),
     title: Joi.string().min(3).required(),
     subject: Joi.string().min(3).required(),
     sentAt: Joi.string().isoDate().required(),
