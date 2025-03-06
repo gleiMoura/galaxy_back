52955ab998555044011245a4f5dcecdd404d8fe5
gleiMoura <gle.bar.mou@gmail.com>
2025-03-06 01:05:10 -0300

    ✨ feat: create a file service

---
+++ src/services/fileService.ts
@@ -0,0 +1,15 @@
+import { generateFileLink } from "repository/filesRepository";
+
+export const createFileLink = async (file) => {
+    if (!file || !file.path || !file.originalname) {
+        throw {
+            response: {
+                status: 404,
+                message: "File or token is not valid or missing required properties"
+            }
+        }
+    }
+
+    const result = await generateFileLink(file);
+    return result;
+};
