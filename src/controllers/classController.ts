ab4b3dc87ab41894e40cd6bbae7cd71550e22e00
gleiMoura <gle.bar.mou@gmail.com>
2025-03-06 15:29:11 -0300

    ✨ feat: create a controller function to get a specific lesson

--- src/controllers/classController.ts
+++ src/controllers/classController.ts
@@ -1,6 +1,6 @@
 import { generateFileLink } from "repository/filesRepository";
 import { Request, Response } from "express";
-import { generateClass } from "services/classService";
+import { generateClass, getSpecificClass } from "services/classService";
 import { createFileLink } from "services/fileService";
 
 export const createClass = async (req: Request, res: Response) => {
@@ -12,4 +12,12 @@ export const createClass = async (req: Request, res: Response) => {
     const newClass = await generateClass(userEmail, data, fileUrl);
 
     res.status(200).send(newClass);
+};
+
+export const getClass = async (req: Request, res: Response) => {
+    const id = req.params?.id;
+
+    const specificClass = await getSpecificClass(id);
+
+    res.status(200).send(specificClass);
 };
