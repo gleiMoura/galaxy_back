58bcf5709621942a77120686c6ebd7cd5f29c4fc
gleiMoura <gle.bar.mou@gmail.com>
2025-03-06 15:29:23 -0300

    ✨ feat: create a repository function to get a specific lesson

--- src/repository/classRepository.ts
+++ src/repository/classRepository.ts
@@ -1,13 +1,55 @@
 import prisma from "config";
 
-export const createClassInDb = async (data) => {
+export const createClassInDb = async (data: any) => {
     try {
         return (
             await prisma.class.create({
-                data
+                data: {
+                    title: data.title,
+                    subject: data.subject,
+                    student: {
+                        connect: { id: parseInt(data.studentId) }
+                    },
+                    teacher: {
+                        connect: { id: parseInt(data.teacherId) }
+                    },
+                    sentAt: data.sentAt,
+                    pdfUrl: data.pdfUrl
+                }
             })
         );
     } catch (error) {
         console.error("Error finding user:", error);
+    }
+};
+
+export const getClassById = async (id: number) => {
+    try {
+        const classData = await prisma.class.findUnique({
+            where: { id },
+            select: {
+                id: true,
+                title: true,
+                subject: true,
+                student: {
+                    select: {
+                        name: true
+                    }
+                },
+                teacher: {
+                    select: {
+                        name: true
+                    }
+                }
+            }
+        });
+
+        if (!classData) {
+            console.log("It's not possible find this Lesson.")
+        };
+
+        return classData;
+    } catch (error) {
+        console.error("Error getting class", error);
     }
 };
