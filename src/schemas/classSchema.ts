33cf3c6bb35d4132059b01d2be8f7175df66b39e
gleiMoura <gle.bar.mou@gmail.com>
2025-03-06 01:02:32 -0300

    ✨ feat: create a schema to class

---
+++ src/schemas/classSchema.ts
@@ -0,0 +1,11 @@
+import Joi from "joi";
+
+const classSchema = Joi.object({
+    studentId: Joi.number().integer().required(),
+    title: Joi.string().min(3).required(),
+    subject: Joi.string().min(3).required(),
+    sentAt: Joi.string().isoDate().required(),
+    pdfUrl: Joi.string().required()
+});
+
+export default classSchema;
