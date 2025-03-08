0befc3cb5415555519ec5f61c9ac4027fcd9d826
gleiMoura <gle.bar.mou@gmail.com>
2025-03-08 20:35:30 -0300

    ✨ feat: create service function to get teacher, teachers and delete teacher

---
+++ src/services/teacherService.ts
@@ -0,0 +1,53 @@
+import { deleteTeacherFromDb, findAllTeachers, findSpecificTeacher } from "repository/teacherRepository";
+
+export const findTeacher = async (user: any, id: number) => {
+    const teacher = findSpecificTeacher(id);
+
+    if (user?.role === "Student") {
+        throw {
+            response: {
+                status: 409,
+                message: "Estudante não pode procurar por um professor."
+            }
+        }
+    }
+
+    if (!teacher) {
+        throw {
+            response: {
+                status: 404,
+                message: "Usuário não foi encontrado no sistema."
+            }
+        }
+    };
+
+    return teacher;
+};
+
+export const findTeachers = async (user: any) => {
+    const teachers = await findAllTeachers();
+
+    if (user?.role === "Student") {
+        throw {
+            response: {
+                status: 409,
+                message: "Estudante não pode procurar por professores!"
+            }
+        }
+    };
+
+    return teachers;
+};
+
+export const deleteSpecificTeacher = async (user: any, teacherId: number) => {
+    await deleteTeacherFromDb(teacherId);
+
+    if (user?.role === "Student") {
+        throw {
+            response: {
+                status: 409,
+                message: "Estudante não pode procurar por professores!"
+            }
+        }
+    };
+};
