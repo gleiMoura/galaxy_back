c25f656e33068f33bee1ae74376457fefe0536e1
gleiMoura <gle.bar.mou@gmail.com>
2025-03-09 20:34:52 -0300

    ✨ feat: create a service function to get payment and all payments

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -1,4 +1,5 @@
-import { createStudentPaymentInDb, createTeacherPaymentInDb } from "repository/paymentRepository";
+import { response } from "express";
+import { createStudentPaymentInDb, createTeacherPaymentInDb, getStudentPaymentInDb, getStudentPaymentsInDb, getTeacherPaymentInDb, getTeacherPaymentsInDb } from "repository/paymentRepository";
 
 export const createNewPayment = async (user: any, data) => {
     if (user?.role !== "Admin") {
@@ -27,4 +28,66 @@ export const createNewPayment = async (user: any, data) => {
             }
         }
     }
+};
+
+export const findPayment = async (user: any, paymentId: string) => {
+    let result = null;
+    const id = parseInt(paymentId);
+
+    if (!paymentId || !id) {
+        throw {
+            response: {
+                status: 404,
+                message: "Id é necessário para achar pagamento"
+            }
+        }
+    }
+
+    if (user?.role === "Student") {
+        result = await getStudentPaymentInDb(id);
+    } else if (user?.role === "Teacher") {
+        result = await getTeacherPaymentInDb(id);
+    } else if(user?.role === "Admin") {
+        result = {
+            student: await getStudentPaymentInDb(id),
+            teacher: await getTeacherPaymentInDb(id)
+        }
+    }
+
+    if (!result) {
+        throw {
+            response: {
+                status: 500,
+                message: "Não foi possível encontrar o pagamento no momento."
+            }
+        }
+    }
+
+    return result;
+};
+
+export const findPayments = async (user: any) => {
+    let result = null;
+
+    if (user?.role === "Student") {
+        result = await getStudentPaymentsInDb();
+    } else if (user?.role === "Teacher") {
+        result = await getTeacherPaymentsInDb();
+    } else if (user?.role === "Admin") {
+        result = {
+            student: await getStudentPaymentsInDb(),
+            teacher: await getTeacherPaymentsInDb()
+        }
+    }
+
+    if (!result) {
+        throw {
+            response: {
+                status: 500,
+                message: "Não foi possível encontrar o pagamento no momento."
+            }
+        }
+    }
+
+    return result;
 };
