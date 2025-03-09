914cc723d043625fc9b086187fdd899bc3e029fc
gleiMoura <gle.bar.mou@gmail.com>
2025-03-09 20:37:43 -0300

    ✨ feat: create student payment routes

--- src/routes/studentRouter.ts
+++ src/routes/studentRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { deleteStudent, getStudent, updateStudent, getStudents } from "controllers/studentController";
 import tokenValidator from "middlewares/tokenValidator.js";
-import { createPayment } from "controllers/paymentController";
+import { createPayment, getPayment, getPayments } from "controllers/paymentController";
 import schemaValidator from "middlewares/schemaValidator";
 import studentPaymentSchema from "schemas/studentPaymentSchema";
 
@@ -12,8 +12,8 @@ studentRouter.get("/students", tokenValidator, getStudents);
 studentRouter.put("/student", tokenValidator, updateStudent);
 studentRouter.delete("/student/:id", tokenValidator, deleteStudent);
 //payment
-studentRouter.post("/student/payment", tokenValidator, schemaValidator(studentPaymentSchema), createPayment);
-//studentRouter.get("/student/payment", tokenValidator, getStudentPayment);
-//studentRouter.get("/student/payment", tokenValidator, getStudentPayments)
+studentRouter.post("/payment/student", tokenValidator, schemaValidator(studentPaymentSchema), createPayment);
+studentRouter.get("/payment/student/:id", tokenValidator, getPayment);
+studentRouter.get("/payments/student", tokenValidator, getPayments)
 
 export default studentRouter;
