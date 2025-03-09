fce4d6568b1b4fe6171faee18d48b0cdaaf58683
gleiMoura <gle.bar.mou@gmail.com>
2025-03-09 19:38:01 -0300

    ♻️ refactor: updated routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,13 @@ import registerRouter from "./registerRouter.js";
 import studentRouter from "./studentRouter.js";
 import contractRouter from "./contractRouter.js";
 import classesRouter from "./classesRoutes.js";
+import teacherRouter from "./teacherRouter.js";
 
 const router = Router();
 router.use(loginRouter);
 router.use(registerRouter);
 router.use(studentRouter);
+router.use(teacherRouter)
 router.use(contractRouter);
 router.use(classesRouter);
 
