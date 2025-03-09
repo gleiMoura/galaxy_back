f04d24f30773608d8ec363d8a77793bc4ee08512
gleiMoura <gle.bar.mou@gmail.com>
2025-03-09 20:37:55 -0300

    ✨ feat: create teacher payment routes

--- src/routes/teacherRouter.ts
+++ src/routes/teacherRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import tokenValidator from "middlewares/tokenValidator.js";
 import { deleteTeacher, getTeacher, getTeachers } from "controllers/teacherController.js";
-import { createPayment } from "controllers/paymentController";
+import { createPayment, getPayment, getPayments } from "controllers/paymentController";
 import schemaValidator from "middlewares/schemaValidator";
 import teacherPaymentSchema from "schemas/teacherPaymentSchema";
 
@@ -12,9 +12,9 @@ teacherRouter.get("/teacher/:id", tokenValidator, getTeacher);
 teacherRouter.get("/teachers", tokenValidator, getTeachers);
 teacherRouter.delete("/teacher", tokenValidator, deleteTeacher);
 //payments
-teacherRouter.post("/teacher/payment", tokenValidator, schemaValidator(teacherPaymentSchema), createPayment)
-//teacherRouter.get("/teacher/payment", tokenValidator, getPayment);
-//teacherRouter.get("/teacher/payments", getPayments);
+teacherRouter.post("/payment/teacher", tokenValidator, schemaValidator(teacherPaymentSchema), createPayment)
+teacherRouter.get("/payment/teacher/:id", tokenValidator, getPayment);
+teacherRouter.get("/payments/teacher", tokenValidator, getPayments);
 
 
 
