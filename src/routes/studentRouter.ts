96bd0a461b6e0d0755db532393de05ebf11ba165
gleiMoura <gle.bar.mou@gmail.com>
2025-03-09 19:28:23 -0300

    ✨ feat: create a router function to create a payment

--- src/routes/studentRouter.ts
+++ src/routes/studentRouter.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { deleteStudent, getStudent, updateStudent, getStudents } from "controllers/studentController";
 import tokenValidator from "middlewares/tokenValidator.js";
+import { createPayment } from "controllers/paymentController";
+import schemaValidator from "middlewares/schemaValidator";
+import studentPaymentSchema from "schemas/studentPaymentSchema";
 
 const studentRouter = Router();
 
-studentRouter.get("/student", tokenValidator, getStudent);
+studentRouter.get("/student/:id", tokenValidator, getStudent);
 studentRouter.get("/students", tokenValidator, getStudents);
 studentRouter.put("/student", tokenValidator, updateStudent);
 studentRouter.delete("/student/:id", tokenValidator, deleteStudent);
+//payment
+studentRouter.post("/student/payment", tokenValidator, schemaValidator(studentPaymentSchema), createPayment);
+//studentRouter.get("/student/payment", tokenValidator, getStudentPayment);
+//studentRouter.get("/student/payment", tokenValidator, getStudentPayments)
 
 export default studentRouter;
