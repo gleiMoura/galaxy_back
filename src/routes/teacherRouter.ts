309d2937a48131ec3be4d6d85d898b5735d0bbd3
gleiMoura <gle.bar.mou@gmail.com>
2025-03-09 19:38:53 -0300

    ♻️ refactor: put route teacher payment to word and update the routes get from it

--- src/routes/teacherRouter.ts
+++ src/routes/teacherRouter.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import tokenValidator from "middlewares/tokenValidator.js";
 import { deleteTeacher, getTeacher, getTeachers } from "controllers/teacherController.js";
+import { createPayment } from "controllers/paymentController";
+import schemaValidator from "middlewares/schemaValidator";
+import teacherPaymentSchema from "schemas/teacherPaymentSchema";
 
 
 const teacherRouter = Router();
 
-teacherRouter.get("/teacher", tokenValidator, getTeacher);
+teacherRouter.get("/teacher/:id", tokenValidator, getTeacher);
 teacherRouter.get("/teachers", tokenValidator, getTeachers);
 teacherRouter.delete("/teacher", tokenValidator, deleteTeacher);
 //payments
-//teacherRouter.put("/teacher/payment", tokenValidator, createTeacherPayment)
+teacherRouter.post("/teacher/payment", tokenValidator, schemaValidator(teacherPaymentSchema), createPayment)
 //teacherRouter.get("/teacher/payment", tokenValidator, getPayment);
 //teacherRouter.get("/teacher/payments", getPayments);
 
