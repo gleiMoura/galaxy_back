0856a9df124f2ff65852345806443263434c14c9
gleiMoura <gle.bar.mou@gmail.com>
2025-03-09 20:34:24 -0300

    ✨ feat: create a controller function to get payment and all payments

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createNewPayment } from "services/paymentService";
+import { createNewPayment, findPayment, findPayments } from "services/paymentService";
 
 export const createPayment = async (req: Request, res: Response) => {
     const data = req.body;
@@ -8,4 +8,21 @@ export const createPayment = async (req: Request, res: Response) => {
     await createNewPayment(user, data);
 
     res.status(200).send("Created!");
+};
+
+export const getPayment = async (req: Request, res: Response) => {
+    const paymentId = req.params?.id;
+    const user = req.user;
+
+    const payment = await findPayment(user, paymentId);
+
+    res.status(200).send(payment);
+};
+
+export const getPayments = async (req: Request, res: Response) => {
+    const user = req.user;
+
+    const payment = await findPayments(user);
+
+    res.status(200).send(payment);
 };
