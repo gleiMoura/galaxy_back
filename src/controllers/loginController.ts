1743751aff64acc88f4c7d44f6acfb8567624249
gleiMoura <gle.bar.mou@gmail.com>
2026-02-11 22:37:11 -0300

    🐛fix: change the import way

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import signinUser from "../services/loginService.js";
+import { signinUser } from "../services/loginService";
 
 export const doLogin = async (req: Request, res: Response) => {
     const credentials = req.body;
 
     const userInformation = await signinUser(credentials);
 
-    res.send(userInformation).status(201);
+    res.status(201).send(userInformation)
 }
