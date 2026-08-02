35c98497424c57b4afb9f5ba2110c8d1e397a2c4
gleiMoura <gle.bar.mou@gmail.com>
2026-08-02 11:50:22 -0300

    ♻️ refactor: improve login schema with more error details

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -2,8 +2,13 @@ import { loginType } from "../interfaces/index.js";
 import joi, { ObjectSchema } from "joi";
 
 const loginSchema: ObjectSchema<loginType> = joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().required(),
+  email: joi.string().email().trim().lowercase().required().messages({
+    "string.email": "Forneça um e-mail válido.",
+    "string.empty": "O e-mail é obrigatório."
+  }),
+  password: joi.string().required().messages({
+    "string.empty": "A senha é obrigatória."
+  }),
 });
 
 export default loginSchema;
