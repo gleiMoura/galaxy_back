1dc7d18d82f5dbc822143aaa5e7fc274933830a3
gleiMoura <gle.bar.mou@gmail.com>
2026-08-02 13:30:33 -0300

    ♻️ refactor: change cloud bucket into two differente buckets: puclic and private

--- src/config/cloud.ts
+++ src/config/cloud.ts
@@ -1,16 +1,10 @@
 import { Storage } from '@google-cloud/storage';
-import { env } from './env'; // Importa nossas variáveis já validadas pelo Zod
+import { env } from './env';
 
-/**
- * Instancia o cliente do Google Cloud Storage.
- *
- * Em produção/Cloud Run: O Workload Identity Federation ou a Service Account
- * anexada ao serviço autentica automaticamente sem arquivo JSON (Application Default Credentials - ADC).
- *
- * Em desenvolvimento local: Basta rodar 'gcloud auth application-default login' no terminal uma única vez.
- */
 export const storage = new Storage({
-  projectId: env.GOOGLE_PROJECT_ID, // Zod garante que nunca será 'undefined'
+  projectId: env.GOOGLE_PROJECT_ID,
 });
 
-export const bucket = storage.bucket(env.GOOGLE_BUCKET_NAME); // Zod garante que nunca será 'undefined'
+// Buckets isolados por finalidade de segurança
+export const publicBucket = storage.bucket(env.GCP_PUBLIC_BUCKET);
+export const privateBucket = storage.bucket(env.GCP_PRIVATE_BUCKET);
