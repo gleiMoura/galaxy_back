99af37307fa552e5a57cdc415b3334dda85c79d7
gleiMoura <gle.bar.mou@gmail.com>
2026-08-02 11:49:08 -0300

    ♻️ refactor: improve controller

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,10 +1,13 @@
-import { Request, Response } from "express";
-import { signinUser } from "../services/loginService";
+import { Request, Response, NextFunction } from "express";
+import { signinUser } from "../services/loginService.js";
 
-export const doLogin = async (req: Request, res: Response) => {
+export const doLogin = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
     const credentials = req.body;
-
     const userInformation = await signinUser(credentials);
 
-    res.status(201).send(userInformation)
-}
+    res.status(200).json(userInformation);
+  } catch (error) {
+    next(error);
+  }
+};
